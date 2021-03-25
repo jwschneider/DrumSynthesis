02d76cbcd97450755b3ab44940642ed65e0e7121
John Schneider <johnschneider@gmx.com>
2021-03-25 09:54:26 -0500

    Added static display widgets for BPM, NotesNum, NotesDenom, MS

--- src/ClockAdvance/ClockAdvancePanel.cpp
+++ src/ClockAdvance/ClockAdvancePanel.cpp
@@ -1,10 +1,34 @@
 #include "ClockAdvance.hpp"
 #include "../common/Components.hpp"
+#include <sstream>
+#include <iomanip>
 
 using namespace rack;
 using namespace clockAdvance;
 
 extern Plugin* plugininstance;
+
+class IntegerDisplayWidget : public TransparentWidget {
+    public:
+    std::string *value = new std::string("---");
+    std::shared_ptr<Font> font;
+    IntegerDisplayWidget() {
+        font = APP->window->loadFont(asset::plugin(pluginInstance, "res/clacon.ttf"));
+    }
+    void draw(const DrawArgs &args) override {
+        if (!value)
+            return;
+        nvgFontSize(args.vg, 20);
+        nvgFontFaceId(args.vg, font->handle);
+        nvgTextLetterSpacing(args.vg, 2.5);
+        std::stringstream to_display;   
+        to_display << std::setw(3) << *value;
+        Vec textPos = Vec(0.f, 14.5f);
+        NVGcolor textcolor = nvgRGB(0x16, 0xb2, 0x16);
+        nvgFillColor(args.vg, textcolor);
+        nvgText(args.vg, textPos.x, textPos.y, to_display.str().c_str(), NULL);
+    }
+};
 
 clockAdvance::ClockAdvancePanel::ClockAdvancePanel(ClockAdvance *module) {
     setModule(module);
@@ -24,12 +48,32 @@ clockAdvance::ClockAdvancePanel::ClockAdvancePanel(ClockAdvance *module) {
 
     addOutput(createOutputCentered<DSOPort>(mm2px(Vec(20.955, 118.431)), module, ClockAdvance::TRIGGER_OUTPUT));
 
+    IntegerDisplayWidget *BPMDisplay = new IntegerDisplayWidget();
+    BPMDisplay->box.pos = mm2px(Vec(15.24, 26.458));
+    BPMDisplay->box.size = mm2px(Vec(11.43, 5.292));
+    //BPMDisplay->value = &placeholder;
+    addChild(BPMDisplay);
     // mm2px(Vec(11.43, 5.292))
-    addChild(createWidget<Widget>(mm2px(Vec(15.24, 26.458))));
+    //addChild(createWidget<Widget>(mm2px(Vec(15.24, 26.458))));
+
+    IntegerDisplayWidget *NotesNumDisplay = new IntegerDisplayWidget();
+    NotesNumDisplay->box.pos = mm2px(Vec(15.24, 46.302));
+    NotesNumDisplay->box.size = mm2px(Vec(11.43, 5.292));
+    addChild(NotesNumDisplay);
     // mm2px(Vec(11.43, 5.292))
-    addChild(createWidget<Widget>(mm2px(Vec(15.24, 46.302))));
+    //addChild(createWidget<Widget>(mm2px(Vec(15.24, 46.302))));
+
+    IntegerDisplayWidget *NotesDenomDisplay = new IntegerDisplayWidget();
+    NotesDenomDisplay->box.pos = mm2px(Vec(15.24, 56.885));
+    NotesDenomDisplay->box.size = mm2px(Vec(11.43, 5.292));
+    addChild(NotesDenomDisplay);
     // mm2px(Vec(11.43, 5.292))
-    addChild(createWidget<Widget>(mm2px(Vec(15.24, 56.885))));
+    //addChild(createWidget<Widget>(mm2px(Vec(15.24, 56.885))));
+
+    IntegerDisplayWidget *MSDisplay = new IntegerDisplayWidget();
+    MSDisplay->box.pos = mm2px(Vec(15.24, 83.344));
+    MSDisplay->box.size = mm2px(Vec(11.43, 5.292));
+    addChild(MSDisplay);
     // mm2px(Vec(11.43, 5.292))
-    addChild(createWidget<Widget>(mm2px(Vec(15.24, 83.344))));
+    //addChild(createWidget<Widget>(mm2px(Vec(15.24, 83.344))));
 }
